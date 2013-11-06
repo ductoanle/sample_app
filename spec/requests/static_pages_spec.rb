7b0b56f846cb7de648bda87b1ac0693bb0f6152a
ethan.le <ductoanle@gmail.com>
2013-11-07 01:26:34 +0800

    finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,37 +1,28 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-
+  subject { page }
   describe "Home Page" do
-    it "should have h1 home" do
-      visit "/static_pages/home"
-      page.should have_selector("h1", :text => "Home")
-    end
-    it "should have the title home" do
-      visit "/static_pages/home"
-      page.should have_selector("title", :text => "Ruby on Rails tutorial Sample App")
-    end
+    before {visit root_path}
+    it { should have_selector("h1", :text => "Welcome to Sample App") }
+    it { should have_selector("title", :text => "Ruby on Rails tutorial Sample App | Home") }
   end
 
   describe "Help page" do
-    it "should have the h1 Help" do
-      visit "/static_pages/help"
-      page.should have_selector("h1", :text => "Help")
-    end
-    it "should have the title Help" do
-      visit "/static_pages/help"
-      page.should have_selector("title", :text => "Ruby on Rails tutorial Sample App")
-    end
+    before {visit help_path}
+    it {should have_selector("h1", :text => "Help")}
+    it {should have_selector("title", :text => "Ruby on Rails tutorial Sample App | Help")}
   end
 
   describe "About Us page" do
-    it "should have the h1 About Us" do
-      visit "/static_pages/about"
-      page.should have_selector("h1", :text => "About")
-    end
-    it "should have the title About Us" do
-      visit "/static_pages/about"
-      page.should have_selector("title", :text => "Ruby on Rails tutorial Sample App")
-    end
+    before {visit about_path}
+    it {should have_selector("h1", :text => "About")}
+    it {should have_selector("title", :text => "Ruby on Rails tutorial Sample App | About")}
+  end
+
+  describe "Contact page" do
+    before {visit contact_path}
+    it {should have_selector("h1", :text => "Contact")}
+    it {should have_selector("title", :text => "Ruby on Rails tutorial Sample App | Contact")}
   end
 end
