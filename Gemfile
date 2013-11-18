152f5aa11c736a7d9d8d1ab9e88366492472fcc4
ethan.le <ductoanle@gmail.com>
2013-11-18 13:32:54 +0800

    WIP: modelling users

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.13'
-gem 'pg'
+gem 'pg', '0.16.0'
 gem 'bootstrap-sass', '2.0.0'
 gem 'bcrypt-ruby'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   before_save {|user| user.email.downcase!}
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
 
   validates :name, presence:true, length: {maximum: 50}
   validates :email, presence:true, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :password, presence:true, length: {minimum: 6}
+  validates :password_confirmation, presence:true
 end
